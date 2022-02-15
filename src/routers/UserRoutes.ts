b16c4a6dd4c7fdf76dddf578cec809041e90825a
Agung Wicaksono Putro <C090821003@Supporting.corp.bankmandiri.co.id>
2022-02-15 19:37:08 +0700

    middleware

--- src/routers/UserRoutes.ts
+++ src/routers/UserRoutes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import BaseRoutes from "./BaseRouter";
 import UserController from "../cotrollers/UserController";
+import { auth } from "../middlewares/AuthMiddleware";
 
 class UserRoutes extends BaseRoutes {
     routes(): void {
-        this.router.get("/", UserController.index);
+        this.router.get("/", auth ,UserController.index);
         this.router.post("/", UserController.create);
         this.router.get("/:id", UserController.show)
         this.router.put("/:id", UserController.update)
